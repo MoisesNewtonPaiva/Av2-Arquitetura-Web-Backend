ba337bdfad9c6e7504f3d20940dbc3c04fe6c753
MoisesNewtonPaiva <12123005@aluno.newtonpaiva.br>
2025-11-14 16:34:36 -0300

    Incluindo o projeto

---
+++ src/test/java/com/pratica/AvProject2/AvProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratica.AvProject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
